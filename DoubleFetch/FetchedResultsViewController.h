56479ccc584c7aaf7a1dd4d2f04fc4c6d202f42a
KANEUCHI Tetsuya <kaneuchi@gmail.com>
2012-01-18 00:01:23 +0900

    Add FetchedResultsViewController

---
+++ DoubleFetch/FetchedResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FetchedResultsViewController.h
+//  DoubleFetch
+//
+//  Created by Kaneuchi Tetsuya on 12/01/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FetchedResultsViewController : UITableViewController
+
+@end
